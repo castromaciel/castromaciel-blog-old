7f174819b575c4bca17d4ca9c4404a7ec84b4308
castromaciel <castromaciel00@gmail.com>
2023-04-02 17:19:52 -0300

    feat: add statics

---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const navItems = [
+  {
+    label: 'home',
+    path: '/'
+  },
+  {
+    label: 'about me',
+    path: '/about-me/'
+  },
+  {
+    label: 'my work',
+    path: '/my-work/'
+  },
+  {
+    label: 'contact',
+    path: '/contact/'
+  }
+]
