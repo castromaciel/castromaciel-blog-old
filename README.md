9dd3e16e223d207755ab182c80ea13bcb86db03f
castromaciel <castromaciel00@gmail.com>
2023-04-02 14:54:07 -0300

    Initial setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# [castromaciel-blog.com](https://castromaciel-blog.vercel.app)
+
+> My personal blog and website.
+
+*The website is based in astro.*
---
+++ src/config.mjs
@@ -0,0 +1,7 @@
+export const SITE = {
+	name: 'Castro Maciel',
+	origin: 'https://castromaciel-blog.vercel.app',
+	basePathname: '/',
+	title: 'Castro Maciel Blog',
+	description: 'Fullstack developer, Stack MERN',
+};
