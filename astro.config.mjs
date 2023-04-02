f91ad6a77d4dc5f28c0803dceb383223652804f8
castromaciel <castromaciel00@gmail.com>
2023-04-02 15:25:14 -0300

    feat: update configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
 import { defineConfig } from 'astro/config';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { SITE } from './src/config.mjs';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: SITE.origin,
+	srcDir: './src',
+  vite: {
+		resolve: {
+			alias: {
+				'~': path.resolve(__dirname, './src'),
+			},
+		},
+	},
+});
