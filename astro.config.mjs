186ebfe56ede1342490adbf37d30e90cb48293e5
castromaciel <castromaciel00@gmail.com>
2023-04-04 22:43:35 -0300

    fix: update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { SITE } from './src/config.mjs';
-import react from "@astrojs/react";
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-
-// https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      langs: [],
+      wrap: true,
+    },
+  },
   site: SITE.origin,
   srcDir: './src',
   vite: {
