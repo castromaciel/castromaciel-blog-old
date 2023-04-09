02997bf2760852015a405ad2a581d20d6d01eb2c
castromaciel <castromaciel00@gmail.com>
2023-04-08 22:16:59 -0300

    fix: change project links

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -35,44 +35,50 @@ export const SOCIAL_LINKS = [
 export const PROJECT_LINKS = [
   {
     description: 'Is a project to simplify the process of creating a new project. It&apos;s contains Eslint, tests with jest and react testing library, MIT License.',
-    externalUrl: 'https://github.com/castromaciel/template-vite-react-ts',
+    repositoryUrl: 'https://github.com/castromaciel/template-vite-react-ts',
     isFinished: true,
-    projectTitle: 'template-vite -react-ts'
+    title: 'template-vite-react-ts',
+    techs: ['react-ts', 'vite', 'vitest', 'scss'],
+    linkDemo: 'https://vite-template-react-ts.netlify.app/'
   },
   {
     description: 'Like before, that template is for Node with typescript. Include express, cors and helmet libraries.',
-    externalUrl: 'https://github.com/castromaciel/template-node-express-ts',
+    repositoryUrl: 'https://github.com/castromaciel/template-node-express-ts',
     isFinished: true,
-    projectTitle: 'template-node-express-ts'
+    title: 'template-node-express-ts',
+    techs: ['react-ts', 'express', 'node', 'mongodb'],
+    linkDemo: 'https://github.com/castromaciel/template-node-express-ts'
   },
   {
     description: 'Have you ever spent hours looking for an api that returns a shopping cart item? Or one of movies and series? Well, the idea of this project is an api, which contains the endpoints of many apis. These apis, in turn, return an array of objects with the necessary data to practice.',
-    externalUrl: 'https://github.com/castromaciel/APIs-collection',
+    repositoryUrl: 'https://github.com/castromaciel/APIs-collection',
     isFinished: false,
-    projectTitle: 'APIs-collection'
+    title: 'APIs-collection',
+    techs: ['react-ts', 'express', 'node', 'mongodb'],
+    linkDemo: 'https://api-rest-server-collection.onrender.com'
   },
   {
     description: 'Is a set of UI components for React.',
-    externalUrl: 'https://github.com/castromaciel/ui-rollingcode',
+    repositoryUrl: 'https://github.com/castromaciel/ui-rollingcode',
     isFinished: false,
-    projectTitle: 'ui-rc-components'
-  },
-  {
-    description: 'This project arises with the idea of creating a page that contains information on SOLID principles and clean code.I use ReactMarkdown to simplify writing documentation.',
-    externalUrl: 'https://github.com/castromaciel/solid-and-clean-code',
-    isFinished: false,
-    projectTitle: 'solid-and-clean-code'
+    title: 'ui-rc-components',
+    techs: ['react-ts', 'storybook', 'scss', 'jest', 'testing library'],
+    linkDemo: 'https://www.npmjs.com/package/ui-rc-components'
   },
   {
     description: 'Basic project to practice tanstack query. The project consumes github api - facebook issues.',
-    externalUrl: 'https://github.com/castromaciel/github-issues-app',
+    repositoryUrl: 'https://github.com/castromaciel/github-issues-app',
     isFinished: true,
-    projectTitle: 'github-issues-app'
+    title: 'github-issues-app',
+    techs: ['react-ts', 'vite', 'react-query', 'bootstrap'],
+    linkDemo: 'https://castromaciel.github.io/github-issues-app/#/issues/list'
   },
   {
     description: 'The objective of the application is to create a React application using Mapbox. It allows us to carry out a search for places, locate ourselves in the selected place on the map, generate a line with the nearest route.',
-    externalUrl: 'https://github.com/castromaciel/maps-app',
+    repositoryUrl: 'https://github.com/castromaciel/maps-app',
     isFinished: true,
-    projectTitle: 'maps-app'
+    title: 'maps-app',
+    techs: ['react-ts', 'vite', 'bootstrap', 'scss', 'mapbox-gl'],
+    linkDemo: 'https://castromaciel.github.io/maps-app/'
   }
 ]
